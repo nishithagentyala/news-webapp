4b6dcd1ec388723200c825432ba8a3ff9ac74058
nishithagentyala <nishithagentyala@gmail.com>
2024-12-21 12:36:06 +0530

    changes made

--- components/NewsApi.js
+++ components/NewsApi.js
@@ -1,6 +1,6 @@
 export async function NewsApi() {
   const news = await fetch(
-    `https://newsapi.org/v2/everything?q=india&from=2024-11-03&sortBy=publishedAt&apiKey=b7b54353da044c9ebb6eefe4bb46963e`
+    `https://newsapi.org/v2/everything?q=india&from=2024-11-03&sortBy=publishedAt&apiKey=${process.env.API_key}`
   );
   const data = await news.json();
 
